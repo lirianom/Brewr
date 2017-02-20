8aabd08d0b012ce516b56455a127ad44c3ca9239
jacob3richard <richardje1@appstate.edu>
2017-02-20 18:15:52 -0500

    Login page completed

--- app/src/main/java/com/example/richardje1/brewr/MainActivity.java
+++ app/src/main/java/com/example/richardje1/brewr/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.richardje1.brewr;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by richardje1 on 2/20/17.
+ */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity{
+
+    Button loginButton;
+    EditText enterUsername, enterPassword;
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button)findViewById(R.id.login_button);
+        enterUsername = (EditText)findViewById(R.id.enter_username_text);
+        enterPassword = (EditText)findViewById(R.id.enter_password_text);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (enterUsername.getText().toString().equals("admin") &&
+                        enterPassword.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
     }
 }
