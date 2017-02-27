d3d650c0ca37ef6d68b4cd41be521b48f44180a4
jacob3richard <richardje1@appstate.edu>
2017-02-27 18:10:29 -0500

    added homepage

--- app/src/main/java/com/example/richardje1/brewr/MainActivity.java
+++ app/src/main/java/com/example/richardje1/brewr/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.richardje1.brewr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity{
 
     Button loginButton;
+    Button makeAccountButton;
     EditText enterUsername, enterPassword;
 
     protected void onCreate(Bundle savedInstanceState){
@@ -22,22 +24,36 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
 
         loginButton = (Button)findViewById(R.id.login_button);
+        makeAccountButton = (Button)findViewById(R.id.make_account_button);
         enterUsername = (EditText)findViewById(R.id.enter_username_text);
         enterPassword = (EditText)findViewById(R.id.enter_password_text);
-        
+
 
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (enterUsername.getText().toString().equals("admin") &&
-                        enterPassword.getText().toString().equals("admin")) {
+                if (enterUsername.getText().toString().equals("a") &&
+                        enterPassword.getText().toString().equals("a")) {
                     Toast.makeText(getApplicationContext(),
                             "Login Successful", Toast.LENGTH_SHORT).show();
+
+                    Intent myIntent = new Intent(v.getContext(), HomePageAllActivity.class);
+                    startActivityForResult(myIntent, 0);
                 } else {
                     Toast.makeText(getApplicationContext(), "Wrong Credentials",
                             Toast.LENGTH_SHORT).show();
 
                 }
+            }
+        });
+
+        makeAccountButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "Make Account Screen",
+                            Toast.LENGTH_SHORT).show();
+
+
             }
         });
 
---
+++ app/src/main/java/com/example/richardje1/brewr/ScreenSlidePageFragment.java
@@ -0,0 +1,27 @@
+package com.example.richardje1.brewr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by richardje1 on 2/24/17.
+ */
+
+public class ScreenSlidePageFragment extends Fragment {
+    public static final String ARG_OBJECT = "object";
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+
+}
+
