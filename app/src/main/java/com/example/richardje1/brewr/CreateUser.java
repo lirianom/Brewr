ae4872c71d152d88798f61f7bfc266ab99d71361
lirianom <martin.liriano@gmail.com>
2017-04-13 21:00:18 -0400

    Some database stuff

--- app/src/main/java/com/example/richardje1/brewr/CreateUser.java
+++ app/src/main/java/com/example/richardje1/brewr/CreateUser.java
@@ -8,17 +8,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.UUID;
+
 /**
  * Created by richardje1 on 4/10/17.
  */
 
 public class CreateUser extends Activity {
     BrewrUser brewrUser;
-    EditText enterFname, enterLname, enterUsername, enterPassword,
+    EditText enterFname, enterLname, enterUsername, enterPassword, passwordConfirm,
             enterEmail;
     Button createButton;
 
-    String fName, lName, username, password, email;
+    BackgroundWorker backgroundWorker;
+
+    String fName, lName, username, password, passwordConf, email;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +32,45 @@ public class CreateUser extends Activity {
         enterLname = (EditText) findViewById(R.id.edit_lname);
         enterUsername = (EditText) findViewById(R.id.edit_username);
         enterPassword = (EditText) findViewById(R.id.edit_password);
+        passwordConfirm = (EditText) findViewById(R.id.confirm_password);
         enterEmail = (EditText) findViewById(R.id.edit_email);
         createButton = (Button) findViewById(R.id.make_account_button);
 
+        /*
         fName = enterFname.getText().toString();
         lName = enterLname.getText().toString();
         username = enterUsername.getText().toString();
+        passwordConf = passwordConfirm.getText().toString();
+        password = enterPassword.getText().toString();
+        email = enterEmail.getText().toString();
+        */
+
+        backgroundWorker = new BackgroundWorker(this);
+    }
+
+    public void onClick(View view) {
+        fName = enterFname.getText().toString();
+        lName = enterLname.getText().toString();
+        username = enterUsername.getText().toString();
+        passwordConf = passwordConfirm.getText().toString();
         password = enterPassword.getText().toString();
         email = enterEmail.getText().toString();
 
 
+        if (!passwordConf.equals(password)) {
+            Intent myIntent = new Intent(getApplicationContext(), CreateUser.class);
+            startActivity(myIntent);
+        }
+        else {
+            String type = "0";
+            UUID uuid = UUID.randomUUID();
+
+            String id = uuid.toString();
+
+            backgroundWorker.execute(type, username, password, id, fName, lName, email);
+        }
+
+    }
         /*8
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +94,5 @@ public class CreateUser extends Activity {
         });
 **/
 
-    }
+
 }
